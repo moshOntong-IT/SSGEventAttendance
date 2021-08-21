9a21c8f6baf5eea74c99c2b25ab295f1fc58cdd3
moshOntong-IT <moshOntong@gmail.com>
2021-08-21 13:12:25 +0800

    Add All files

---
+++ src/main/java/org/codx/MainApp.java
@@ -0,0 +1,35 @@
+package org.codx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("mainFXML.fxml"));
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(root);
+//        scene.setFill(Color.TRANSPARENT);
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
