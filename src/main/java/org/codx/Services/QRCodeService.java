e56de4ef66dd7f66a956acb1c6a0d05b04f917dd
moshOntong-IT <moshOntong@gmail.com>
2021-09-22 08:47:37 +0800

    Add QR code service

---
+++ src/main/java/org/codx/Services/QRCodeService.java
@@ -0,0 +1,43 @@
+package org.codx.Services;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class QRCodeService {
+
+
+    //qr code encoder
+    private static void generateQRCode(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix matrix = writer.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(matrix, "JPG", path);
+    }
+
+
+    //decode through Image Webcam
+    public String decodeQRCode(BufferedImage image) {
+        LuminanceSource source = new BufferedImageLuminanceSource(image);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+}
