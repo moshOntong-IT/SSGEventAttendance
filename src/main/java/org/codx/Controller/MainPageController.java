362f9a0f1cb9f472b9b0615b42394950d17ea777
moshOntong-IT <moshOntong@gmail.com>
2021-09-26 11:16:52 +0800

    Add event pane in main page

--- src/main/java/org/codx/Controller/MainPageController.java
+++ src/main/java/org/codx/Controller/MainPageController.java
@@ -4,18 +4,28 @@ import javafx.beans.Observable;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainPageController implements Initializable {
 
@@ -47,7 +57,7 @@ public class MainPageController implements Initializable {
     private StackPane stackLayout;
 
     @FXML
-    private Pane pink;
+    private Pane eventPane;
 
     @FXML
     private Pane red;
@@ -91,7 +101,7 @@ public class MainPageController implements Initializable {
         departmentIcon.setImage(imagePath.get(3));
 
         if (event.getSource() == eventButton) {
-            pink.toFront();
+            eventPane.toFront();
             eventIcon.setImage(imageHighlightPath.get(0));
             eventButton.getStyleClass().clear();
             eventButton.getStyleClass().add("menu-nav-left-btn-highlight");
@@ -126,5 +136,36 @@ public class MainPageController implements Initializable {
     void exit(ActionEvent event) {
         System.exit(0);
     }
+
+    @FXML
+    void attend(ActionEvent event) {
+        Parent root = departmentButton.getScene().getRoot();
+        ColorAdjust adj = new ColorAdjust(0, 0, -0.8, 0);
+        GaussianBlur blur = new GaussianBlur(10);
+        adj.setInput(blur);
+        root.setEffect(adj);
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("eventPage.fxml"));
+            Parent form = loader.load();
+//            AnnounceFormController controller = loader.getController();
+//            controller.setUserInformation(userinfo);
+//            controller.SetFullName(userinfo.getFullName());
+            Stage stage = new Stage();
+            Scene scene = new Scene(form);
+            form.requestFocus();
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.UNDECORATED);
+
+            stage.showAndWait();
+
+            root.setEffect(null);
+
+        } catch (IOException ex) {
+            Logger.getLogger(MainPageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 
 }
